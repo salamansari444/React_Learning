7a11ccff70e70ed8a58ec3d1b9f74d3fafb01c79
salamansari444 <salamansari444@gmail.com>
2023-11-07 10:50:52 +0530

    sec commit

---
+++ 01basicreact/src/Youtube.jsx
@@ -0,0 +1,8 @@
+
+function Youtube(){
+    return(
+        <h1>Classic Youtube App</h1>
+    )
+}
+
+export default Youtube
---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,15 @@
+import Youtube from "./Youtube"
+
+
+function App() {
+
+
+  return (
+    <>
+    <h1>Classic vite App</h1>
+    <Youtube/>
+    </>
+  )
+}
+
+export default App
